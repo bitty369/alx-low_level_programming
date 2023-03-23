3b5c3753ffeb10a792964a174c8dc8f33a6d6c3b
bitty369 <ibtissamkadiasch@gmail.com>
2023-03-23 16:10:40 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper.c - Check if a lettre is supper
+ * 0x: The number to be checkout
+ * Return: 1 for supper letter of 0 for any else
+ */
+
+int _isupper(int x)
+{
+if ( x >= 65 && x <= 0)
+{
+return(1);
+}
+return(0);
+}
