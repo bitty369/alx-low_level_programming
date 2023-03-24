4e4978eaa1106dcabe45084c4d83909f5fce73b6
bitty369 <ibtissamkadiasch@gmail.com>
2023-03-24 05:30:23 -0700

    ./1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,15 +3,15 @@
 /**
 * _isdigit - Check if c is between 0 - 9
 *
-*0c: input
+* @c: input
 *
 * Return: 1 for upper letter. 0 if not
 */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 if (c >= 48 && c <= 57)
 return (1);
 else
 return (0);
- }
+}
