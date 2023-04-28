befbf40ed70e1361862ea67d8b60aa55ce5885bd
bitty369 <ibtissamkadiasch@gmail.com>
2023-04-28 13:02:16 -0700

    singly linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,30 +1,33 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h> /* size_t, printf */
-#include <stdlib.h> /* malloc */
-#include <string.h> /* strdup */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long size_t;
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
+* struct node - strucutre of linked list we will use
+* @str: string format
+* @len: length
+* @next: pointer to next node
+*/
+
+typedef struct node
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct node *next;
 } list_t;
 
-/* function prototypes for all files */
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void) __attribute__ ((constructor));
 
-#endif /* LIST_H */
+
+#endif
